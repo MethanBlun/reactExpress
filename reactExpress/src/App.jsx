bce5ac13495358e290ba25abfdaa51967167618e
Osirus jack <methanblun@gmail.com>
2024-02-08 13:56:45 +0000

    Counters that update separately

--- reactExpress/src/App.jsx
+++ reactExpress/src/App.jsx
@@ -1,33 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      {/* <h1>Vite + React</h1> */}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div>
-        <button onClick={() => setCount((count)=>count - count) }>reset</button>
-      </div>
-   
-    </>
-  )
+
+
+function App() {
+return (
+  <div>
+    <h1>Counters that update separately</h1>
+    <MyButton />
+    <MyButton />
+  </div>
+);
 }
+
+function MyButton() {
+const [count, setCount] = useState(0);
+
+function handleClick() {
+  setCount(count + 1);
+}
+
+return (
+  <button onClick={handleClick}>
+    Clicked {count} times
+  </button>
+);
+}
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+
+//   return (
+    
+//     <>
+//     function MyButton(params) {
+      
+      
+//     }
+
+    
+//   function handleClick(){
+//     setCount(count + 1)
+//   }
+
+
+//       <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+   
+//     </>
+//   )
+// }
 
 export default App
