07ff0e8000ec8380acbd56d4e5ce36bce80358cf
Osirus Jack <methanblun@gmail.com>
2024-02-10 14:55:48 +0000

    style.css wrks

--- reactExpress/src/App.jsx
+++ reactExpress/src/App.jsx
@@ -1,60 +1,10 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+
+//import "./styles.css"
+  export default function App() {
 
 
-
-
-function App() {
-return (
-  <div>
-    <h1>Counters that update separately</h1>
-    <MyButton />
-    <MyButton />
-  </div>
-);
-}
-
-function MyButton() {
-const [count, setCount] = useState(0);
-
-function handleClick() {
-  setCount(count + 1);
-}
-
-return (
-  <button onClick={handleClick}>
-    Clicked {count} times
-  </button>
-);
+    return 'hi'
 }
 
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-
-//   return (
-    
-//     <>
-//     function MyButton(params) {
-      
-      
-//     }
-
-    
-//   function handleClick(){
-//     setCount(count + 1)
-//   }
-
-
-//       <button onClick={handleClick}>
-//       Clicked {count} times
-//     </button>
-   
-//     </>
-//   )
-// }
-
-export default App
+ 
