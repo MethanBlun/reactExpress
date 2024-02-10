b59801674100b835f7824f9bcbbec26cbb6e3697
Osirus Jack <methanblun@gmail.com>
2024-02-10 20:05:19 +0000

    static todo list from tutorial hell

--- reactExpress/src/App.jsx
+++ reactExpress/src/App.jsx
@@ -3,9 +3,37 @@ import './index.css'
 
 
  export default  function App() {
- 
+return(
+<>
 
-  return 'Hi'
+    <form className='new-item-form'>
+    <div  className='form-row'>
+      <label htmlFor="item">New item</label>
+      <input type="text" id='item' />
+    </div>
+    <button className='btn'>Add</button>
+    </form>
+    <h1 className='header'> Todo List</h1>
+    <ul className='list'>
+
+      <li>
+        <label >
+          <input type="checkbox" />
+          item1
+        </label>
+        <button className='btn btn-danger'>delete</button>
+      </li>
+    </ul>
+
+
+
+ </>
+  
+)
+
+
+
+   
 }
 
  
